800728f73050943871c82554a2f4351b756aea11
memekbaumemek <58591416+memekbaumemek@users.noreply.github.com>
2019-12-10 00:43:45 +0700

    Add files via upload

---
+++ list.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+while true; do
+
+python main.py +6282245348471
+sleep 5
+python main.py +6285334935615
+sleep 5
+python main.py +6282331323902
+sleep 5
+python main.py +17855720153
+sleep 5
+python main.py +17068106591
+sleep 5
+python main.py +14176950593
+sleep 5
+python main.py +19563175898
+sleep 5
+python main.py +18659278727
+sleep 5
+python main.py +16183628760
+sleep 5
+python main.py +19563471815
+sleep 5
+python main.py +17072789140
+sleep 5
+python main.py +13159791382
+sleep 5
+python main.Py +18063050682
+sleep 5
+python main.py +18052627243
+sleep 5
+python main.py +18059789670
+sleep 5
+python main.py +19805532189
+sleep 5
+python main.py +18023951044
+sleep 5
+python main.py +18043265137
+sleep 5
+python main.py +18044824738
+sleep 5
+python main.py +18059792378
+sleep 5
+python main.Py +18042389564
+sleep 5
+python main.py +18023224369
+sleep 5
+python main.py +18044788495
+done
